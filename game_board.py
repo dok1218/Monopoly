f48e8d26293490503bdcfff46e83becdb3d618f2
dok1218 <73504942+dok1218@users.noreply.github.com>
2020-11-07 11:48:19 +0000

    Changed the code to iterate over different rent amounts (base rent, with 1 house, with hotel, etc) and to store the result in a new text file for each iteration.

--- game_board.py
+++ game_board.py
@@ -3,6 +3,7 @@
 
 # import modules needed for the code
 import random
+import numpy as np
 # import openpyxl # currentyly there is a windows bug preventing openpyxl module from working
 
 # define the number of games to play and how many dice throws each game should have, also defines the variable "board"
@@ -11,7 +12,6 @@ import random
 n_games = 100
 n_throws = 120
 board = []
-#rent = []
 house = 0
 hotel = 0
 streets = [11, 13, 14]
@@ -21,47 +21,23 @@ percentage_hits = []
 
 # create list which will store the total money earnt from rent on selected owned properties
 for _ in range(0, 40):
-#    rent.append(0)
     rent_amount.append(0)
 
 # create lists to represent the Chance and Community chest cards and the corresponding positions they move to
 Chance = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 Community_chest = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-# create lists to store rents based on number of houses and hotels
-base_rent = [0, 2, 0, 4, 0, 0, 6, 0, 6, 8, 0, 10, 0, 10, 12, 0, 14, 0, 14, 16, 0, 18, 0, 18, 20, 0, 22, 22, 0, 22, 0, 26, 26, 0, 28, 0, 0, 35, 0, 50]
-house_1 = [0, 10, 0, 20, 0, 0, 30, 0, 30, 40, 0, 50, 0, 50, 60, 0, 70, 0, 70, 80, 0, 90, 0, 90, 100, 0, 110, 110, 0, 120, 0, 130, 130, 0, 150, 0, 0, 175, 0, 200]
-house_2 = [0, 30, 0, 60, 0, 0, 90, 0, 90, 100, 0, 150, 0, 150, 180, 0, 200, 0, 200, 220, 0, 250, 0, 250, 300, 0, 330, 330, 0, 360, 0, 390, 390, 0, 450, 0, 0, 500, 0, 600]
-house_3 = [0, 90, 0, 180, 0, 0, 270, 0, 270, 300, 0, 450, 0, 450, 500, 0, 550, 0, 550, 600, 0, 700, 0, 700, 750, 0, 800, 800, 0, 850, 0, 900, 900, 0, 1000, 0, 0, 1100, 0, 1400]
-house_4 = [0, 160, 0, 320, 0, 0, 400, 0, 400, 450, 0, 625, 0, 625, 700, 0, 750, 0, 750, 800, 0, 875, 0, 875, 925, 0, 975, 975, 0, 1025, 0, 1100, 1100, 0, 1200, 0, 0, 1300, 0, 1700]
-with_hotels = [0, 250, 0, 450, 0, 0, 550, 0, 550, 600, 0, 750, 0, 750, 900, 0, 950, 0, 950, 1000, 0, 1050, 0, 1050, 1100, 0, 1150, 1150, 0, 1200, 0, 1275, 1275, 0, 1400, 0, 0, 1500, 0, 2000]
+# create array to store rent amounts 0 = base rent, 1 = 1 house, 2 = 2 houses, 3 = 3 houses, 4 = 4houses, 5 = hotel
+rent = np.array([[0, 2, 0, 4, 0, 0, 6, 0, 6, 8, 0, 10, 0, 10, 12, 0, 14, 0, 14, 16, 0, 18, 0, 18, 20, 0, 22, 22, 0, 22, 0, 26, 26, 0, 28, 0, 0, 35, 0, 50],
+                 [0, 10, 0, 20, 0, 0, 30, 0, 30, 40, 0, 50, 0, 50, 60, 0, 70, 0, 70, 80, 0, 90, 0, 90, 100, 0, 110, 110, 0, 120, 0, 130, 130, 0, 150, 0, 0, 175, 0, 200],
+                 [0, 30, 0, 60, 0, 0, 90, 0, 90, 100, 0, 150, 0, 150, 180, 0, 200, 0, 200, 220, 0, 250, 0, 250, 300, 0, 330, 330, 0, 360, 0, 390, 390, 0, 450, 0, 0, 500, 0, 600],
+                 [0, 90, 0, 180, 0, 0, 270, 0, 270, 300, 0, 450, 0, 450, 500, 0, 550, 0, 550, 600, 0, 700, 0, 700, 750, 0, 800, 800, 0, 850, 0, 900, 900, 0, 1000, 0, 0, 1100, 0, 1400],
+                 [0, 160, 0, 320, 0, 0, 400, 0, 400, 450, 0, 625, 0, 625, 700, 0, 750, 0, 750, 800, 0, 875, 0, 875, 925, 0, 975, 975, 0, 1025, 0, 1100, 1100, 0, 1200, 0, 0, 1300, 0, 1700],
+                 [0, 250, 0, 450, 0, 0, 550, 0, 550, 600, 0, 750, 0, 750, 900, 0, 950, 0, 950, 1000, 0, 1050, 0, 1050, 1100, 0, 1150, 1150, 0, 1200, 0, 1275, 1275, 0, 1400, 0, 0, 1500, 0, 2000]])
 
-# set the rent value for owned properties
-rent = base_rent # sets the rent value for all properties
-# if (house == 0) and (hotel == 0):
-#     for x in range(len(streets)):
-#         y = streets[x]
-#         rent[y] = base_rent[y]
-# elif house == 1:
-#     for x in range(len(streets)):
-#         y = streets[x]
-#         rent[y] = house_1[y]
-# elif house == 2:
-#     for x in range(len(streets)):
-#         y = streets[x]
-#         rent[y] = house_2[y]
-# elif house == 3:
-#     for x in range(len(streets)):
-#         y = streets[x]
-#         rent[y] = house_3[y]
-# elif house == 4:
-#     for x in range(len(streets)):
-#         y = streets[x]
-#         rent[y] = house_4[y]
-# elif hotel == 1:
-#     for x in range(len(streets)):
-#         y = streets[x]
-#         rent[y] = with_hotels[y]
+# creates list to store percentage hits
+for _ in range(0, 40):
+    percentage_hits.append(0)
 
 # function to deal with moving to the nearest station card
 def nearest_station(position_x):
@@ -127,45 +103,49 @@ def community_card(position_x):
 for _ in range(0, 40):
     board.append(0)
 
+file_name = ["base_rent.txt", "1_house.txt", "2_houses.txt", "3_houses.txt", "4_houses.txt", "hotel.txt"]
+
 # code to run the game
-for _ in range(n_games):
-    random.shuffle(Chance) # shuffles the Chance and Community chest cards
-    random.shuffle(Community_chest)
-    position = 0 # sets the starting position for the game
-    for _ in range(n_throws):
-        dice_sum = random.randint(1, 6) + random.randint(1, 6) # stores the sum of two random integers betweem 1 and 6
-        if (position + dice_sum) <= 39:
-            if (position + dice_sum) == 30: # scenario where "go to jail" is landed on
-                position = 11
-            else:
-                position = position + dice_sum  # updates the new postion of the piece
+for i in range(0, 5):
+    for _ in range(n_games):
+        random.shuffle(Chance)  # shuffles the Chance and Community chest cards
+        random.shuffle(Community_chest)
+        position = 0  # sets the starting position for the game
+        for _ in range(n_throws):
+            dice_sum = random.randint(1, 6) + random.randint(1,6)  # stores the sum of two random integers betweem 1 and 6
+            if (position + dice_sum) <= 39:
+                if (position + dice_sum) == 30:  # scenario where "go to jail" is landed on
+                    position = 11
+                else:
+                    position = position + dice_sum  # updates the new postion of the piece
+                    community_card(position)  # checks if the new position is a community card and if so, draws and applies effect
+                    chance_card(position)  # checks if the new position is a chance card and if so, draws and applies effect
+                board[position] = board[position] + 1  # logs that the new position was landed on
+                rent_amount[position] = rent_amount[position] + rent[i, position]  # logs the amount of rent incurred from landing on the position
+            elif (position + dice_sum) > 39:  # deals with the case when piece has moved all the way around the board
+                position = position + dice_sum - 40  # resets the board
                 community_card(position)  # checks if the new position is a community card and if so, draws and applies effect
                 chance_card(position)  # checks if the new position is a chance card and if so, draws and applies effect
-            board[position] = board[position] + 1  # logs that the new position was landed on
-            rent_amount[position] = rent_amount[position] + rent[position] # logs the amount of rent incurred from landing on the position
-        elif (position + dice_sum) >39: # deals with the case when piece has moved all the way around the board
-            position = position + dice_sum -40 # resets the board
-            community_card(position) # checks if the new position is a community card and if so, draws and applies effect
-            chance_card(position) # checks if the new position is a chance card and if so, draws and applies effect
-            board[position] = board[position] + 1
-            rent_amount[position] = rent_amount[position] + rent[position] # logs the amount of rent incurred from landing on the position
+                board[position] = board[position] + 1
+                rent_amount[position] = rent_amount[position] + rent[i, position]  # logs the amount of rent incurred from landing on the position
 
-# calculate the percentage of total positions landed on each street constitutes
-for _ in range(0, 40):
-    percentage_hits.append(0)
-for i in range(len(board)):
-    total_hits = total_hits + board[i]
-for i in range(len(percentage_hits)):
-    percentage_hits[i] = 100 * (board[i]/total_hits)
+    # calculate the percentage of total positions landed on each street constitutes
+    for x in range(len(board)):
+        total_hits = total_hits + board[x]
+    for y in range(len(percentage_hits)):
+        percentage_hits[y] = 100 * (board[y] / total_hits)
 
-# store results in an excel worbook, for now will use text file instead
-#wb = openpyxl.Workbook() # creates a new workbook in the root folder and opens it, comment out if using a pre existing sheet
-f = open("base_rent.txt","w") #creates a text file
-f.close() # closes the newly created text file
-with open("base_rent.txt", "w") as br:
-    br.write("\n".join(str(w) for w in rent_amount)) # writes the list rent_amount to txt file line by line
+    # store results in a text file
+    f = open(file_name[i], "w")  # creates a text file
+    f.close()  # closes the newly created text file
+    with open(file_name[i], "w") as f:
+        f.write("\n".join(str(w) for w in rent_amount))  # writes the list rent_amount to txt file line by line
 
-# print(percentage_hits)
-# print(position)
-# print(Chance)
-# print(Community_chest)
+    # set all variables to 0 for the next iteration of the code (total_hits, board, rent_amount, percentage_hits)
+    total_hits = 0
+    for z in range(len(board)):
+        board[z] = 0
+    for a in range(len(rent_amount)):
+        rent_amount[a] = 0
+    for b in range(len(percentage_hits)):
+        percentage_hits[b] = 0
